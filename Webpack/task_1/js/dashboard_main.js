f84867290f5bd19bda65990e4f3023d2d2ed639f
denver <denvercayetano23@gmail.com>
2025-02-08 01:37:34 +0000

    Webpack

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import { debounce } from 'lodash';
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$(document).ready(function() {
+  // Add elements to the body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  // Bind debounced updateCounter to button click
+  $('button').on('click', debounce(updateCounter, 250));
+});
