d3c512f1eec3b8ec6b9d80d61f179baf5bff7b99
denver <denvercayetano23@gmail.com>
2025-02-08 01:30:23 +0000

    Webpack

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+  // Add three paragraphs to the body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
