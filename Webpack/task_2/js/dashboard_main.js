be97dfce2ff1db3ce3a5b60b21ecfbeeaf978d40
denver023 <denvercayetano23@gmail.com>
2025-02-07 17:52:24 -0800

    Webpack

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -7,21 +7,19 @@ function updateCounter() {
   document.getElementById('count').innerHTML = `${count} clicks on the button`;
 }
 
-// Create button
+// Create elements
 const button = document.createElement('button');
 button.innerHTML = 'Click here to get started';
-button.addEventListener('click', updateCounter);
+button.onclick = updateCounter;
 
-// Create counter element
 const counter = document.createElement('span');
 counter.id = 'count';
 counter.innerHTML = '0 clicks on the button';
 
-// Create logo element
 const logo = document.createElement('div');
 logo.id = 'logo';
 
-// Add elements to the page
+// Add elements to page
 document.body.appendChild(logo);
 document.body.appendChild(button);
 document.body.appendChild(counter);
