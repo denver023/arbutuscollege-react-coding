8bf221667ca7ca6002623e03066e03e7fe8847a4
denver023 <denvercayetano23@gmail.com>
2025-02-07 18:20:45 -0800

    Update dashboard_main.js

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -10,16 +10,12 @@ function updateCounter() {
 }
 
 $(document).ready(function() {
-  // Add logo element
   $('body').append('<div id="logo"></div>');
-  
-  // Add other elements to the body
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
   $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
 
-  // Bind debounced updateCounter to button click
   $('button').on('click', _.debounce(updateCounter, 500));
 });
